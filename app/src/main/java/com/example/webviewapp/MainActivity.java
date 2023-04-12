83bd97d883aa5cce13d368ff985bf07e7977bc28
d22hanol <d22hanol@student.his.se>
2023-04-12 10:51:11 +0200

    linked buttons to show-functions

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showInternalWebPage(){
-
+        myWebView.loadUrl("file:///android_asset/demo.html");
     }
 
     @Override
@@ -81,11 +81,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
